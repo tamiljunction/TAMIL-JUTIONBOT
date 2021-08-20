77272d80ff2f9b342f88ccc7e664e06d30184a61
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-20 20:01:16 +0000

    Create utils.py

---
+++ superos/utils.py
@@ -0,0 +1,184 @@
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import os
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from var import Var
+
+from userbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
+from userbot.helpers.exceptions import CancelProcess
+from userbot.Config import Config
+from userbot import bot
+from telethon import events
+from pathlib import Path
+from var import Var
+from userbot import LOAD_PLUG
+from userbot import CMD_LIST, SUDO_LIST
+import logging
+import inspect
+import asyncio
+from traceback import format_exc
+from time import gmtime, strftime
+import subprocess
+import sys
+import traceback
+import datetime
+
+from telethon.tl.functions.messages import GetPeerDialogsRequest
+
+from typing import List
+
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    from userbot.Config import Config
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Config
+
+def command(**args):
+    args["func"] = lambda e: e.via_bot_id is None
+
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    if 1 == 0:
+        return print("stupidity at its best")
+    else:
+        pattern = args.get("pattern", None)
+        allow_sudo = args.get("allow_sudo", None)
+        allow_edited_updates = args.get('allow_edited_updates', False)
+        args["incoming"] = args.get("incoming", False)
+        args["outgoing"] = True
+        if bool(args["incoming"]):
+            args["outgoing"] = False
+
+        try:
+            if pattern is not None and not pattern.startswith('(?i)'):
+                args['pattern'] = '(?i)' + pattern
+        except:
+            pass
+
+        reg = re.compile('(.*)')
+        if not pattern == None:
+            try:
+                cmd = re.search(reg, pattern)
+                try:
+                    cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+                except:
+                    pass
+
+                try:
+                    CMD_LIST[file_test].append(cmd)
+                except:
+                    CMD_LIST.update({file_test: [cmd]})
+            except:
+                pass
+
+        if allow_sudo:
+            args["from_users"] = list(Config.SUDO_USERS)
+            # Mutually exclusive with outgoing (can only set one of either).
+            args["incoming"] = True
+        del allow_sudo
+        try:
+            del args["allow_sudo"]
+        except:
+            pass
+        
+        args["blacklist_chats"] = True
+        black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
+        if len(black_list_chats) > 0:
+            args["chats"] = black_list_chats
+
+        if "allow_edited_updates" in args:
+            del args['allow_edited_updates']
+
+        def decorator(func):
+            if allow_edited_updates:
+                bot.add_event_handler(func, events.MessageEdited(**args))
+            bot.add_event_handler(func, events.NewMessage(**args))
+            try:
+                LOAD_PLUG[file_test].append(func)
+            except:
+                LOAD_PLUG.update({file_test: [func]})
+            return func
+
+        return decorator
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import userbot.utils
+        import sys
+        import importlib
+        from pathlib import Path
+        path = Path(f"userbot/plugins/{shortname}.py")
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("𝕷єgєи∂𝕭οτ 2.o " + shortname)
+    else:
+        import userbot.utils
+        import sys
+        import importlib
+        from pathlib import Path
+        path = Path(f"userbot/plugins/{shortname}.py")
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.tgbot = bot.tgbot
+        mod.Var = Var
+        mod.command = command
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = userbot.utils
+        mod.Config = Config
+        mod.borg = bot
+        mod.LEGENDBOT = bot
+        mod.edit_or_reply = edit_or_reply
+        mod.delete_LEGEND = delete_LEGEND
+        # support for LEGENDBOT originals
+        sys.modules["LEGENDBOT.utils"] = userbot.utils
+        sys.modules["LEGENDBOT"] = userbot
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = userbot.utils
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["userbot.plugins." + shortname] = mod
+        LOGS.info("𝕷єgєи∂𝕭οτ 2.o " + shortname)
+
+
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in LOAD_PLUG[shortname]:
+                bot.remove_event_handler(i)
+            del LOAD_PLUG[shortname]
+
+        except BaseException:
+            name = f"userbot.plugins.{shortname}"
+
+            for i in reversed(range(len(bot._event_builders))):
+                ev, cb = bot._event_builders[i]
+                if cb.__module__ == name:
+                    del bot._event_builders[i]
+    except BaseException:
+        raise ValueError
+
+
