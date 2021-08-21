f483829dc64b4118ce586f30cede2044b200ffc7
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-21 09:47:52 +0000

    Delete userbot directory

--- userbot/plugins/__init__.py
+++
@@ -1,5 +0,0 @@
-from superos import *
-from superos.utils import *
-from superos.Config.Config import Config
-from superos.helpers.functions import *
-from superos.supercmds.cmdhelp import CmdHelp
--- userbot/plugins/assistant/pmbot/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import *
-from superos import ALIVE_NAME
-from superos import *
-OWNER_NAME = ALIVE_NAME
-OWNER_ID = bot.uid
