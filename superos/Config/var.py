b2570b999738cc44378732ee7d3d010a599b67a3
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-21 09:47:18 +0000

    Delete superos directory

--- superos/Config/var.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ENV = bool(os.environ.get("ENV", False))
-
-if ENV:
-    from superos.Config.legend_config import Config
-else:
-    if os.path.exists("config.py"):
-        from superos.Config.Config import Development as Var
