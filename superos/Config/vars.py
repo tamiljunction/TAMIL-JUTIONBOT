ffc43630bf1407de5a59ed4b50683b21c0cf441c
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-21 13:51:24 +0530

    Update vars.py

--- superos/Config/vars.py
+++ superos/Config/vars.py
@@ -6,4 +6,4 @@ if ENV:
     from superos.Config.legend_config import Config
 else:
     if os.path.exists("config.py"):
-        from config import Development as Config
+        from superos.Config import Development as Config
