4206dbdf51c5873e68355a7a9f0d2c0a5718d9c1
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-21 13:46:57 +0530

    Update __init__.py

--- superos/__init__.py
+++ superos/__init__.py
@@ -5,10 +5,10 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient
 from superos.helpers import functions as simpdef
 from superos.Config.Config import Config
-from var import Var
+from . import Var
 
 StartTime = time.time()
-LEGENDversion = "2.ο"
+LEGENDversion = "⚡2.O⚡"
 botversion = "1.ο"
 
 os.system("pip install --upgrade pip")
