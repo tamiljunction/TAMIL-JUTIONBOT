d8557fd85c1f5dc830798c57dafea5a0f755c33d
LEGEND-OS <LEGEND-OS@users.noreply.github.com>
2021-08-21 08:23:23 +0000

    Initial commit

--- superos/Config/exampleconfig.py
+++ superos/Config/exampleconfig.py
@@ -1,4 +1,4 @@
-from superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.Config.sample_config import Config
+from superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.superos.Config.sample_config import Config
 
 
 class Development(Config):
