c7cc979d93c51478670afb983ab8297391554355
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-21 22:23:19 +0530

    Update actressdp.py

--- userbot/plugins/actressdp.py
+++ userbot/plugins/actressdp.py
@@ -107,6 +107,6 @@ async def autopic(event):
             await asyncio.sleep(60)
         except:
             return
-CmdHelp("actressdp").add_command(
+CmdHelp("𝚊𝚌𝚝𝚛𝚍𝚙").add_command(
        'actressdp', None, 'Starts autodp of Actress'
 ).add()
