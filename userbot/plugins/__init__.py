54693045eb251549b28dc967a66d5b4d747184d6
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-22 11:40:53 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -15,7 +15,7 @@ LEGEND_logo1 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo2 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo4 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo3 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-LEGENDversion = ♥️𝚅2.𝙾♥️
+LEGENDversion = "♥️𝚅2.𝙾♥️"
 
 async def get_user_id(ids):
     if str(ids).isdigit():
