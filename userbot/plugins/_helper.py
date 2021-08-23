85993f17259fe50576cd04ed6367b3d162cb8ffe
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-23 21:16:59 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -3,9 +3,74 @@ import asyncio
 
 import requests
 from telethon import functions
-
+from . import *
 from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+@bot.on(admin_cmd(pattern="op ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="op ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    tgbotusername = Config.BOT_USERNAME
+    chat = "@Botfather"
+    if tgbotusername is not None:
+        try:
+            results = await event.client.inline_query(tgbotusername, "hellbot_help")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except noinline:
+            hell = await eor(event, "**Inline Mode is disabled.** \n__Turning it on, please wait for a minute...__")
+            async with bot.conversation(chat) as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(tgbotusername)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message(perf)
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    return await hell.edit("Unblock @Botfather first.")
+                await hell.edit(f"**Turned On Inline Mode Successfully.** \n\nDo `{hl}help` again to get the help menu.")
+            await bot.delete_messages(
+                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id]
+            )
+    else:
+        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
+
+
+@bot.on(admin_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
+async def hellbott(event):
+    if event.fwd_from:
+        return
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await eor(event, str(CMD_HELP[args]))
+        else:
+            await eod(event, "**⚠️ Error !** \nNeed a module name to show plugin info.")
+    else:
+        string = ""
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
+        for i in sayfa:
+            string += f"`▶️ `"
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await eod(event, "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
 
 
 @bot.on(admin_cmd(pattern="op ?(.*)", outgoing=True))
