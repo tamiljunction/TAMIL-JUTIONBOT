f23483fa234bff8c0a729109d4b00899b9426d19
LEGEND-OS <LEGEND-OS@users.noreply.github.com>
2021-08-24 13:57:55 +0000

    Initial commit

--- userbot/Config.py
+++ userbot/Config.py
@@ -105,7 +105,7 @@ if ENV:
         NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 2))
         # emoji to be displayed  in help .legend
         EMOJI_IN_HELP1 = os.environ.get("EMOJI_IN_HELP1", "⚜️ ")
-        EMOJI_IN_HELP111111112 = os.environ.get("EMOJI_IN_HELP111111112", "🌹 ")
+        EMOJI_IN_HELP2 = os.environ.get("EMOJI_IN_HELP2", "🌹 ")
         # specify command handler that should be used for the plugins
         # this should be a valid "regex" pattern
         COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r"\.")
