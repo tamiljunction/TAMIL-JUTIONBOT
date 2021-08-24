05aef37d1b9274ec12f644551a8166348d4baf19
LEGEND-OS <LEGEND-OS@users.noreply.github.com>
2021-08-24 14:07:07 +0000

    Initial commit

--- userbot/config/legendconfig.py
+++ userbot/config/legendconfig.py
@@ -61,7 +61,7 @@ class Config(object):
     AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
     PMSECURITY = os.environ.get("PMSECURITY", "ON")
     # for autopic
-    EMOJI_IN_HELP11111 = os.environ.get("EMOJI_IN_HELP11111", "🔱 ")
+    EMOJI_IN_HELP1 = os.environ.get("EMOJI_IN_HELP1", "🔱 ")
     EMOJI_IN_HELP11112 = os.environ.get("EMOJI_IN_HELP11112", "🔱 ")
     AUTOPIC_TEXT = os.environ.get(
         "AUTOPIC_TEXT", "Life Is too Short.\n And so is your TG account."
