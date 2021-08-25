715c2152f76ed3420ea44c7449f50a8db089afc8
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-26 03:14:15 +0530

    Update actiondp.py

--- userbot/plugins/actiondp.py
+++ userbot/plugins/actiondp.py
@@ -105,6 +105,6 @@ async def autopic(event):
             await asyncio.sleep(60)
         except:
             return
-CmdHelp("𝚊𝚌𝚝𝚒𝚘𝚗𝚍𝚙").add_command(
+CmdHelp("actiondp").add_command(
        'actiondp', None, 'Starts autodp of Action Hero & Some Actress Pic'
 ).add()
