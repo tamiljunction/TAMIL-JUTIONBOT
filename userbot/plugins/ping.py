3cdb3d37348851789b85e634c81b2ba3d6c6668d
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-29 07:41:27 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -68,10 +68,10 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
-        f"**♦️𝙿𝚘𝚗𝚐!♦️**\n\n🧬{ms}\n🔥𝙼𝚢 𝙾𝚠𝚗𝚎𝚛 [{DEFAULTUSER}](tg://user?id={legend})\n                  [𝕴tß 𝕷êgëñd𝕭øt 🇮🇳](https://t.me/Legend_Userbot)" 
+        f"**♦️𝙿𝚘𝚗𝚐!♦️**\n⚡️{ms}\n🔥𝙼𝚢 𝙾𝚠𝚗𝚎𝚛 [{DEFAULTUSER}](tg://user?id={legend})"
     )
 
-CmdHelp("𝙿𝚒𝚗𝚐").add_command(
+CmdHelp("Ping").add_command(
   "ping", None, "Shows you the ping speed of server"
 ).add_command(
   "hbping", None, "Shows you the ping speed of server with an animation"
