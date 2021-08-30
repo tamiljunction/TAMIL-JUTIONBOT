7bdf21afbad6c36451f3f53435fda490e61ef918
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-30 19:37:31 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -102,12 +102,13 @@ async def legend_is_on():
 
 # Why not come here and chat??
 #    try:
-#        await bot(JoinChannelRequest("@HellBot_Chat"))
+#        await bot(JoinChannelRequest("@Its_Legendbot"))
 #    except BaseException:
 #        pass
 
 
 bot.loop.create_task(legend_is_on())
+
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
