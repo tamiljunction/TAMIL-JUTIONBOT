4b7c7e7501869e02fe83b7e0f1afc05895656ded
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-31 12:38:58 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -7,7 +7,7 @@ from telegraph import Telegraph, exceptions, upload_file
 from userbot import ALIVE_NAME
 from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
-
+from . import *
 LEGEND_NAME = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND User"
 
 legend = bot.uid
