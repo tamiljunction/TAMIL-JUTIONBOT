3f62bc296275f0356c3a8c2becfbcc44343903f1
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-01 06:51:41 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -13,12 +13,12 @@ WARN_PIC = Config.PM_PIC or "https://telegra.ph/file/58df4d86400922aa32acd.jpg"
 PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 PM_ON_OFF = Config.PM_DATA
-CSTM_PMP = Config.CUSTOM_PM_MSG or "**You Have Trespassed To My Master's PM!\nThis Is Illegal And Regarded As Crime.**"
+CSTM_PMP = Config.PM_MSG or "**You Have Trespassed To My Master's PM!\nThis Is Illegal And Regarded As Crime.**"
 HELL_ZERO = "Go get some sleep retard. \n\n**Blocked !!**"
 HELL_FIRST = (
     "**🔥 Hêllẞø† Prîvã†é Sêçürïty Prø†öçõl 🔥**\n\nThis is to inform you that "
     "{} is currently unavailable.\nThis is an automated message.\n\n"
-    "{}\n\n**Please Choose Why You Are Here!!**".format(hell_mention, CSTM_PMP)
+    "{}\n\n**Please Choose Why You Are Here!!**".format(legend_mention, CSTM_PMP)
 )
 
 @bot.on(admin_cmd(pattern="block$"))
