e47be04c2062720fa2f49653726c27c4a2d2babb
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-04 10:36:15 +0530

    Create songs.py

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -10,7 +10,7 @@ import re
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import bot
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply, progress
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply, progress
 from userbot.cmdhelp import CmdHelp
 from userbot.helpers.functions import deEmojify
 
