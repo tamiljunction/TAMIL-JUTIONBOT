cd28b9972e5d6e2449433671a209501f9a7de0f3
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-04 16:25:56 +0530

    Create song.py

--- userbot/plugins/song.py
+++ userbot/plugins/song.py
@@ -1,7 +1,16 @@
+import re
+import random
+import json
+from pathlib import Path
 import asyncio
+import math
+import os
 import time
 
 from telethon.tl.types import DocumentAttributeAudio
+from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
+from youtube_search import YoutubeSearch
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
     ContentTooShortError,
@@ -13,13 +22,69 @@ from youtube_dl.utils import (
     UnavailableVideoError,
     XAttrMetadataError,
 )
-from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import *
-from userbot.helpers import *
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply, eor
-@bot.on(admin_cmd(pattern="sing ?(.*)"))
-@bot.on(sudo_cmd(pattern="sing ?(.*)", allow_sudo=True))
+LOGO1 = "https://telegra.ph/file/2ecaa4738213cff953388.jpg"
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            await event.edit(
+                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+@bot.on(admin_cmd(pattern="songs ?(.*)"))
+@bot.on(sudo_cmd(pattern="songs ?(.*)", allow_sudo=True))
 async def _(event):
     query = event.text[6:]
     max_results = 1
@@ -28,7 +93,7 @@ async def _(event):
     hell = await eor(event, f"__Searching for__ `{query}`")
     hel_ = await song_search(event, query, max_results, details=True)
     x, title, views, duration, thumb = hel_[0], hel_[1], hel_[2], hel_[3], hel_[4]
-    thumb_name = f'{LOGO1}'
+    thumb_name = f'thumb{LOGO1}.jpg'
     thumbnail = requests.get(thumb, allow_redirects=True)
     open(thumb_name, 'wb').write(thumbnail.content)
     url = x.replace("\n", "")
@@ -75,8 +140,8 @@ async def _(event):
     await hell.delete()
     os.remove(f"{hell_data['id']}.mp3")
 
-@bot.on(admin_cmd(pattern="vsing ?(.*)"))
-@bot.on(sudo_cmd(pattern="vsing ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="vsongs ?(.*)"))
+@bot.on(sudo_cmd(pattern="vsongs ?(.*)", allow_sudo=True))
 async def _(event):
     query = event.text[7:]
     max_results = 1
@@ -85,7 +150,7 @@ async def _(event):
     hell = await eor(event, f"__Searching for__ `{query}`")
     hel_ = await song_search(event, query, max_results, details=True)
     x, title, views, duration, thumb = hel_[0], hel_[1], hel_[2], hel_[3], hel_[4]
-    thumb_name = f'thumb{ForGo10God}.jpg'
+    thumb_name = f'thumb{LOGO1}'
     thumbnail = requests.get(thumb, allow_redirects=True)
     open(thumb_name, 'wb').write(thumbnail.content)
     url = x.replace("\n", "")
