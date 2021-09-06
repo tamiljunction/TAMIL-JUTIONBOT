d1f60e7ff98f57b7ea26522fa825a96fbc4a1cc8
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-06 18:22:29 +0000

    Update and rename removedp_060921184604.py to removedp.py

--- userbot/plugins/removedp.py
+++ userbot/plugins/removedp.py
@@ -4,10 +4,12 @@ from telethon.tl.functions.photos import DeletePhotosRequest
 from . import *
 from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
-@borg.on(admin_cmd(pattern=r"rmdp", outgoing=True))
-@borg.on(sudo_cmd(pattern=r"rmdp$", allow_sudo=True))
+@borg.on(admin_cmd(pattern=r"rmvdp", outgoing=True))
+@borg.on(sudo_cmd(pattern=r"rmvdp$", allow_sudo=True))
 async def _(event):
     pic = await borg.get_profile_photos('me')
     await borg(DeletePhotosRequest(pic))
-    await event.edit("Done!!!")
-CmdHelp("Remove dp").add_command(".rmdp", None, 'Romves dp')
+    await event.edit("Done Ur All pic Had Deleted!!!")
+CmdHelp("removedp").add_command(
+    ".rmvdp", None, 'Remove All DP In One Command'
+).add()
