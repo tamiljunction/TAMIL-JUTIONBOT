29acebb59cc1fd8cf91b3ea0b5ff4636e86fac24
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-07 18:49:37 +0530

    Update angry.py

--- userbot/plugins/angry.py
+++ userbot/plugins/angry.py
@@ -14,6 +14,6 @@ async def _(event):
         await event.edit("".join(deq))
         deq.rotate(1)
         
-CmdHelp("𝚊𝚗𝚐𝚛𝚢").add_command(
+CmdHelp("angry").add_command(
   "🤬", None, "υѕє αи∂ ѕєє"
 ).add()
