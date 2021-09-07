03aafe029b49232688f10440f5996225bdb0bd27
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-07 20:01:56 +0530

    Update memify3.py

--- userbot/plugins/memify3.py
+++ userbot/plugins/memify3.py
@@ -18,48 +18,6 @@ if not os.path.isdir(path):
     os.makedirs(path)
 
 
-@bot.on(admin_cmd(pattern="mmf ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="mmf ?(.*)", allow_sudo=True))
-async def _(event):
-    _reply = await event.get_reply_message()
-    msg = event.pattern_match.group(1)
-    if not (_reply and (_reply.media)):
-        legen_ = await eod(event, "`Can't memify this 🥴`")
-        return
-    legend = await _reply.download_media()
-    if legend.endswith((".tgs")):
-        legen_ = await eor(event, "**Memifying 🌚🌝**")
-        cmd = ["lottie_convert.py", legend, "pic.png"]
-        file = "pic.png"
-        process = await asyncio.create_subprocess_exec(
-            *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await process.communicate()
-        stderr.decode().strip()
-        stdout.decode().strip()
-    elif legend.endswith((".webp", ".png")):
-        legen_ = await eor(event, "**Memifying 🌚🌝**")
-        pics = Image.open(legend)
-        pics.save("pic.png", format="PNG", optimize=True)
-        file = "pic.png"
-    else:
-        legen_ = await eor(event, "**Memifying 🌚🌝**")
-        img = cv2.VideoCapture(legend)
-        tal, semx = img.read()
-        cv2.imwrite("pic.png", semx)
-        file = "pic.png"
-    output = await draw_meme_text(file, msg)
-    await bot.send_file(
-        event.chat_id, output, force_document=False, reply_to=event.reply_to_msg_id
-    )
-    await legen_.delete()
-    try:
-        os.remove(legend)
-        os.remove(file)
-        os.remove(output)
-    except BaseException:
-        pass
-
 
 @bot.on(admin_cmd(pattern="mms ?(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="mms ?(.*)", allow_sudo=True))
