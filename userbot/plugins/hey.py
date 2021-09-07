a9270cb71941fb7079030f64ac43853600671ea9
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-07 18:13:55 +0530

    Update hey.py

--- userbot/plugins/hey.py
+++ userbot/plugins/hey.py
@@ -5,7 +5,7 @@ async def hi(event):
     giveVar = event.text
     ult = giveVar[4:5]
     if not ult:
-        ult = "🌺"
+        ult = "⚜"
     await edit_or_reply(
         event,
         f"{ult}✨✨{ult}✨{ult}{ult}{ult}\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}{ult}{ult}{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨{ult}{ult}{ult}\n☁☁☁☁☁☁☁☁",
