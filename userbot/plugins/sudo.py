f96ac96ab4d4101e9010b81d36997ab2c64f4ed8
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 08:08:56 +0530

    Update sudo.py

--- userbot/plugins/sudo.py
+++ userbot/plugins/sudo.py
@@ -3,20 +3,20 @@ import heroku3
 from telethon.tl.functions.users import GetFullUserRequest
 
 from . import *
-
+from userbot.Config import Config
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
-sudousers = os.environ.get("SUDO_USERS", None)
+sudousers = Config.SUDO_USERS
 
 
 @bot.on(admin_cmd(pattern="sudo"))
 async def sudo(event):
     sudo = "True" if Config.SUDO_USERS else "False"
-    users = os.environ.get("SUDO_USERS", None)
+    users = sudousers
     if sudo == "True":
-        await eod(event, f"📍 **Sudo :**  `Enabled`\n\n📝 **Sudo users :**  `{users}`", 10)
+        await eor(event, f"📍 **Sudo :**  `Enabled`\n\n📝 **Sudo users :**  `{users}`")
     else:
-        await eod(event, f"📍 **Sudo :**  `Disabled`", 7)
+        await eod(event, f"📍 **Sudo :**  `Disabled`")
 
 
 @bot.on(admin_cmd(pattern="addsudo(?: |$)"))
