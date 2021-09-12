eb0a2ab08c4f7391669f0e48d9832a11782a22fa
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 13:49:17 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -5,7 +5,7 @@ from userbot.cmdhelp import CmdHelp
 import datetime
 from telethon import version
 from . import *
-from userbot import ALIVE_NAME
+from userbot.Config import ALIVE_NAME
 LEGEND_USER = bot.me.first_name
 Its_LegendBoy = bot.uid
 USERID = bot.uid
@@ -20,7 +20,7 @@ LEGEND_logo1 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo2 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo4 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
 LEGEND_logo3 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-LEGENDversion = "♥️𝚅2.𝙾♥️"
+LEGENDversion = "𝚅2.𝙾"
 
 perf = "[ †hê Lêɠêɳ̃dẞø† ]"
 DEVLIST = [
