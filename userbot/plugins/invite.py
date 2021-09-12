03c2529997eb43d20072e167f1d9a8da08a8a9fe
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 15:11:38 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -12,7 +12,7 @@ from telethon.tl.functions.messages import GetFullChatRequest
 from telethon.tl.functions.channels import InviteToChannelRequest
 
 from userbot import CMD_HELP
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd, eor
 from userbot.cmdhelp import CmdHelp
 
 async def get_chatinfo(event):
