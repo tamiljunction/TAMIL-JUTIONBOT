83aa86d84184437b9d0c8705f1ebe8859d1d86fc
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 22:22:46 +0530

    Update and rename direct_link.py to directlink.py

--- userbot/plugins/directlink.py
+++ userbot/plugins/directlink.py
@@ -349,6 +349,6 @@ def useragent():
     return user_agent.text
 
 
-CmdHelp("direct_links").add_command(
-  'direct', 'link', 'Reply to a link or paste a URL to generate a direct download link.\nList of supported URLs:-\n• Google Drive\n•Cloud mail\n• Yandex.Disk\n• AFH\n• Zippy Share\n• Media fire\n• SourceForge\n• OSDN\n• Github'
+CmdHelp("directlink").add_command(
+  'directlink', 'link', 'Reply to a link or paste a URL to generate a direct download link.\nList of supported URLs:-\n• Google Drive\n•Cloud mail\n• Yandex.Disk\n• AFH\n• Zippy Share\n• Media fire\n• SourceForge\n• OSDN\n• Github'
 ).add()
