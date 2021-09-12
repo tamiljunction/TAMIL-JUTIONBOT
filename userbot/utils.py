93d2d6d105882984c62d4046726401d590c717bc
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 14:34:54 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -133,7 +133,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        LOGS.info("𝕷єgєи∂𝕭οτ 2.o " + shortname)
+        LOGS.info("𝕷єgєи∂𝕭οτ 2.Ö " + shortname)
     else:
         import userbot.utils
         import sys
