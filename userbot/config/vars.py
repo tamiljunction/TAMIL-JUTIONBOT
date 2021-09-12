39c6700f648fe3a23ea2f72686782c14047545b4
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-12 14:11:33 +0530

    Update vars.py

--- userbot/config/vars.py
+++ userbot/config/vars.py
@@ -5,9 +5,9 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from legendconfig import Config
+    from legend_config import Config
 else:
-    if os.path.exists("legendconfig.py"):
+    if os.path.exists("Config.py"):
         from config import Development as Config
 
 # legendbot
