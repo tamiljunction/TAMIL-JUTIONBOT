48bb2f87c8f689caf1a3578d1900c9be357c9658
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-13 07:14:37 +0530

    Update angry.py

--- userbot/plugins/angry.py
+++ userbot/plugins/angry.py
@@ -8,12 +8,31 @@ async def _(event):
     if event.fwd_from:
         return
     event = await edit_or_reply(event, "𝙸 𝚊𝚖 𝚊𝚗𝚐𝚛𝚢 ")
-    deq = deque(list("😡🔥😡🔥🤬🔥🤬🔥🤬𝚅2.𝙾🔥😡🔥🤬😡🔥🤬🤬🔥😡"))
+    deq = deque(list("😡🔥😡🔥🤬🔥🤬🔥😡🔥🤬😡🔥🤬🤬🔥😡"))
+    for _ in range(100):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+        
+import asyncio
+from collections import deque
+from . import *
+
+@bot.on(admin_cmd(pattern=r"^🤣", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"^🤣", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Lots Of Laugh")
+    deq = deque(list("🤣😂🤣😂🤣😅😂🤣😂🤣😂"))
     for _ in range(100):
         await asyncio.sleep(0.3)
         await event.edit("".join(deq))
         deq.rotate(1)
         
 CmdHelp("angry").add_command(
-  "🤬", None, "υѕє αи∂ ѕєє"
+  "🤬", None, "υѕє αи∂ ѕєє ex - 🤬"
+).add_command(
+  "🤣", None, "Use and See"
 ).add()
+
