061b9c0b8d89b8377cfef69a5e60fc19e151361e
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-17 10:16:00 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -4,19 +4,17 @@ from userbot import ALIVE_NAME
 from userbot import bot
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
 PM_IMG = "https://telegra.ph/file/404bdfb481fba8ca7bef3.jpg"
-pm_caption = "➥ αssísԵαղԵ ís օղlíղҽ \n\n"
-pm_caption += "➥ ѕγѕτєм ѕτατѕ\n"
-pm_caption += "➥ τєℓєτнοи νєяѕιοи: `1.15.0` \n"
-pm_caption += "➥ ℓєgєϐ∂ϐοτ: `3.8.6` \n"
-pm_caption += "➥ ∂αταϐαѕє ѕτατυѕ: `Functional`\n"
-pm_caption += "➥ ϲυяяєиτ ϐяαиϲн: `master`\n"
-pm_caption += f"➥ νєяѕιοи : `2.0`\n"
-pm_caption += f"➥ мγ ϐοѕѕ: {DEFAULTUSER} \n"
-pm_caption += "➥ нєяοκυ ∂αταϐαѕє: `AWS - Working Properly`\n\n"
-pm_caption += "➥ **𝓛𝓲𝓬𝓮𝓷𝓼𝓮** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
-pm_caption += "➥ 𝓒𝓸𝓹𝔂𝓻𝓲𝓼𝓱𝓽 : By [lҽցҽղժճօԵ](https://t.me/Legend_Userbot)\n"
-pm_caption += "[Assistant By ℓεɠεn∂](https://t.me/Its_LegendBoy)"
-
+pm_caption = "『Lêɠêɳ̃dẞø†』Is Ôñĺîne \n\n"
+pm_caption += "➥ Ôwñêř ~ {legend_mention}\n"
+pm_caption += "╭──────────────
+pm_caption += "┣─ Ťêlethon: `1.15.0` \n"
+pm_caption += "┣─ 『Lêɠêɳ̃dẞø†』: `{LEGENDversion}` \n"
+pm_caption += f"┣─ Çhâññel: [Channel](https://t.me/Its_LegendBot) \n"
+pm_caption += "┣─ **License** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
+pm_caption += "┣─ Copyright: By [ 『Lêɠêɳ̃dẞø†』 ](https://t.me/Legend_Userbot)\n"
+pm_caption += "┣─ [Assistant By 『Lêɠêɳ̃dẞøy』](https://t.me/Its_LegendBoy)"
+pm_caption += "╰──────────────
+pm_caption += "»»» [『Lêɠêɳ̃dẞø†』](https://t.me/Legend_Userbot) «««"
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
