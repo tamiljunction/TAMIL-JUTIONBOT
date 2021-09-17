c3b5de83d771ace474948e04ea1ea1922b26018d
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-17 10:49:26 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -8,14 +8,15 @@ pm_caption = "『Lêɠêɳ̃dẞø†』Is Ôñĺîne \n\n"
 pm_caption += "➥ Ôwñêř ~ {legend_mention}\n"
 pm_caption += "╭──────────────
 pm_caption += "┣─ Ťêlethon: `1.15.0` \n"
-pm_caption += "┣─ 『Lêɠêɳ̃dẞø†』: `{LEGENDversion}` \n"
+pm_caption += f"┣─ 『Lêɠêɳ̃dẞø†』: `{LEGENDversion}` \n"
 pm_caption += f"┣─ Çhâññel: [Channel](https://t.me/Its_LegendBot) \n"
-pm_caption += "┣─ **License** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
-pm_caption += "┣─ Copyright: By [ 『Lêɠêɳ̃dẞø†』 ](https://t.me/Legend_Userbot)\n"
-pm_caption += "┣─ [Assistant By 『Lêɠêɳ̃dẞøy』](https://t.me/Its_LegendBoy)"
+pm_caption += f"┣─ **License** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
+pm_caption += f"┣─ Copyright: By [ 『Lêɠêɳ̃dẞø†』 ](https://t.me/Legend_Userbot)\n"
+pm_caption += f"┣─ [Assistant By 『Lêɠêɳ̃dẞøy』](https://t.me/Its_LegendBoy)"
 pm_caption += "╰──────────────"
-pm_caption += "»»» [『Lêɠêɳ̃dẞø†』](https://t.me/Legend_Userbot) «««"
+pm_caption += f"»»» [『Lêɠêɳ̃dẞø†』](https://t.me/Legend_Userbot) «««"
 # only Owner Can Use it
+
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption)
